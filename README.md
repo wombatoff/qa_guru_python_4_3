b8e2a00191852946a8f1b79bb475b641054bbacb
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-02-26 15:37:02 +0300

    qa guru python 4.3

---
+++ README.md
@@ -0,0 +1,47 @@
+# QA GURU Python 4 поток
+### Задание 3
+
+
+
+## Как запустить проект на Windows:
+
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone https://github.com/wombatoff/qa_guru_python_4_3
+```
+
+```
+cd qa_guru_python_4_3
+```
+
+Создать и активировать виртуальное окружение:
+
+```
+python -m venv venv
+source venv/Scripts/activate
+```
+
+
+Установить зависимости из файла requirements.txt:
+
+```
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+## Запуск тест
+
+```
+pytest test_qa_guru_python_4_3.py
+```
+
+
+---
+
+## Технологии проекта
+
+- Python
+- Pytest
+- Selene
+
---
+++ test_qa_guru_python_4_3.py
@@ -0,0 +1,33 @@
+import pytest
+from selene import be, have
+from selene.support.shared import browser
+
+TEST_URL = 'https://demoqa.com/automation-practice-form'
+
+
+@pytest.fixture
+def configured_browser():
+    browser.config.window_width = 1980
+    browser.config.window_height = 1080
+    return browser
+
+
+def test_positive(configured_browser):
+    browser.open(TEST_URL)
+    browser.element('[id="firstName"]').should(be.blank).type('Oleg')
+    browser.element('[id="lastName"]').should(be.blank).type('Greckiy')
+    browser.element('[for="gender-radio-1"]').should(be.clickable).click()
+    browser.element('[id="userNumber"]').should(be.blank).type('1234567890')
+    browser.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    browser.element('[id="submit"]').should(be.clickable).click()
+    assert browser.element('[id="example-modal-sizes-title-lg"]').should(have.text('Thanks for submitting the form'))
+
+
+def test_negative(configured_browser):
+    browser.open(TEST_URL)
+    browser.element('[id="firstName"]').should(be.blank).type('Oleg')
+    browser.element('[id="lastName"]').should(be.blank).type('Greckiy')
+    browser.element('[for="gender-radio-1"]').should(be.clickable).click()
+    browser.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    browser.element('[id="submit"]').should(be.clickable).click()
+    assert browser.element('[id="submit"]').should(be.clickable)
